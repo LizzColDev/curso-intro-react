408899b4b01ca3bf54c99d2ba55c8ac1a0fe4321
LizzColDev <lquintero28@live.com>
2023-02-08 17:21:52 -0500

    Add CompleteTodo, DeleteTodo and SearchTodos functions

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,21 +1,19 @@
 import React from "react";
 import './TodoSearch.css';
 
-function TodoSearch(){
-    const [searchValue, setSearchValue] = React.useState('');
+function TodoSearch({ searchValue, setSearchValue}){
     const onSearchValueChange = (event) => {
         setSearchValue(event.target.value)
     };
 
-    return[
+    return(
         <input 
         className="TodoSearch" 
         placeholder="Search ToDo"
         value={searchValue}
         onChange={onSearchValueChange}
-        />,
-        <p>{searchValue}</p>
-    ];
+        />
+    );
 }
 
 export {TodoSearch};
